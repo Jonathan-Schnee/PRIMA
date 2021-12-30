3d347f6a3bd89bbcb7a2f0fd0b8c749064d6e093
Jonathan Schnee <jonathan.schnee@hs-furtwangen.de>
2021-12-30 13:40:41 +0100

    Added RayCast for jumping

--- RougeDungeon/Script/Build/Script.js
+++ RougeDungeon/Script/Build/Script.js
@@ -38,35 +38,56 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
-    let ctrForward = new ƒ.Control("Forward", 200, 0 /* PROPORTIONAL */);
+    let ctrForward = new ƒ.Control("Forward", 550, 0 /* PROPORTIONAL */);
     ctrForward.setDelay(10);
     let agent;
     let agentRB;
-    let j = 0;
     let isGrounded;
+    let ground;
+    let agentdampT;
     function start(_event) {
         viewport = _event.detail;
+        viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.COLLIDERS;
         let graph = viewport.getBranch();
         agent = graph.getChildrenByName("Agent")[0];
+        ground = graph.getChildrenByName("Ground")[0];
+        generateCG(ground);
         agentRB = agent.getComponent(ƒ.ComponentRigidbody);
         agentRB.effectRotation = new ƒ.Vector3(0, 0, 0);
+        agentdampT = agentRB.dampTranslation;
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
+        isGrounded = false;
+        let direction = ƒ.Vector3.Y(-1);
+        let agentTransL = agent.mtxWorld.translation.clone;
+        agentTransL.x -= agent.getComponent(ƒ.ComponentMesh).mtxPivot.scaling.x / 2 - 0.01;
+        let rayL = ƒ.Physics.raycast(agentTransL, direction, 0.5, true, ƒ.COLLISION_GROUP.GROUP_2);
+        let agentTransR = agent.mtxWorld.translation.clone;
+        agentTransR.x += agent.getComponent(ƒ.ComponentMesh).mtxPivot.scaling.x / 2 - 0.01;
+        let rayR = ƒ.Physics.raycast(agentTransR, direction, 0.5, true, ƒ.COLLISION_GROUP.GROUP_2);
+        if (rayL.hit || rayR.hit) {
+            agentRB.dampTranslation = agentdampT;
+            isGrounded = true;
+        }
         let forward = ƒ.Keyboard.mapToTrit([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT], [ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]);
         ctrForward.setInput(forward);
         agentRB.applyForce(ƒ.Vector3.X(ctrForward.getOutput()));
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
-            j = 50;
-        }
-        if (j != 0) {
-            agentRB.applyForce(ƒ.Vector3.Y(1000));
-            j--;
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE]) && isGrounded) {
+            //agentRB.applyLinearImpulse(ƒ.Vector3.Y(15));
+            agentRB.dampTranslation = 0.1;
+            agentRB.addVelocity(ƒ.Vector3.Y(10));
         }
         ƒ.Physics.world.simulate(); // if physics is included and used
         viewport.draw();
         ƒ.AudioManager.default.update();
+    }
+    function generateCG(ground) {
+        for (let g of ground.getChildren()) {
+            let groundRB = g.getComponent(ƒ.ComponentRigidbody);
+            groundRB.collisionGroup = ƒ.COLLISION_GROUP.GROUP_2;
+        }
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- RougeDungeon/Script/Source/Main.ts
+++ RougeDungeon/Script/Source/Main.ts
@@ -5,43 +5,64 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
-  let ctrForward: ƒ.Control = new ƒ.Control("Forward", 200, ƒ.CONTROL_TYPE.PROPORTIONAL);
+  let ctrForward: ƒ.Control = new ƒ.Control("Forward", 550, ƒ.CONTROL_TYPE.PROPORTIONAL);
   ctrForward.setDelay(10);
 
   let agent: ƒ.Node;
   let agentRB: ƒ.ComponentRigidbody;
-  let j = 0
   let isGrounded: boolean;
+  let ground: ƒ.Node;
+  let agentdampT: number;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
+    viewport.physicsDebugMode = ƒ.PHYSICS_DEBUGMODE.COLLIDERS ;
     let graph: ƒ.Node = viewport.getBranch();
     agent= graph.getChildrenByName("Agent")[0];
-    agentRB = agent.getComponent(ƒ.ComponentRigidbody)
-    agentRB.effectRotation = new ƒ.Vector3(0,0,0)
-
+    ground = graph.getChildrenByName("Ground")[0];
+    generateCG(ground)
+    agentRB = agent.getComponent(ƒ.ComponentRigidbody);
+    agentRB.effectRotation = new ƒ.Vector3(0,0,0);
+    agentdampT = agentRB.dampTranslation
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
+    isGrounded = false
+    let direction = ƒ.Vector3.Y(-1)
+
+    let agentTransL = agent.mtxWorld.translation.clone;
+    agentTransL.x -= agent.getComponent(ƒ.ComponentMesh).mtxPivot.scaling.x /2 - 0.01;
+    let rayL = ƒ.Physics.raycast(agentTransL, direction, 0.5, true, ƒ.COLLISION_GROUP.GROUP_2)
+    let agentTransR = agent.mtxWorld.translation.clone;
+    agentTransR.x += agent.getComponent(ƒ.ComponentMesh).mtxPivot.scaling.x /2 - 0.01;
+    let rayR = ƒ.Physics.raycast(agentTransR, direction, 0.5, true, ƒ.COLLISION_GROUP.GROUP_2)
+    if(rayL.hit || rayR.hit){
+      agentRB.dampTranslation = agentdampT;
+      isGrounded = true
+    }
 
     let forward: number = ƒ.Keyboard.mapToTrit( [ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT], [ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]);
     ctrForward.setInput(forward);
     agentRB.applyForce(ƒ.Vector3.X(ctrForward.getOutput()));
-    
-    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])){
-      j = 50
-    }
 
-    if(j != 0){
-      agentRB.applyForce(ƒ.Vector3.Y(1000));
-      j-- 
+
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE]) && isGrounded){
+      //agentRB.applyLinearImpulse(ƒ.Vector3.Y(15));
+      agentRB.dampTranslation = 0.1
+      agentRB.addVelocity(ƒ.Vector3.Y(10))
     }
 
     ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+  }
+
+  function generateCG(ground: ƒ.Node){
+    for(let g of ground.getChildren()){
+      let groundRB = g.getComponent(ƒ.ComponentRigidbody)
+      groundRB.collisionGroup = ƒ.COLLISION_GROUP.GROUP_2
+    }
   }
 }
